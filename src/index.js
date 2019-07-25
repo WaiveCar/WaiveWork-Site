04f65c3b149ac73b6b61383cecf7ffb48156ec6f
David Alexander Leigh <daleighan@gmail.com>
2019-07-25 14:54:30 -0700

    Added setup for a redux store

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('different text');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import App from './components/App.jsx';
+import store from './store';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
