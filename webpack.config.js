9e36820c7be770969e0684458f48f1020d577627
David Alexander Leigh <daleighan@gmail.com>
2019-07-25 13:53:39 -0700

    Initial commit with webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {loader: 'style-loader'},
+          {loader: 'css-loader'},
+          {loader: 'sass-loader'},
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [{loader: 'style-loader'}, {loader: 'css-loader'}],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    inline: true,
+    port: 8080,
+    historyApiFallback: {
+      index: '/',
+    },
+  },
+};
