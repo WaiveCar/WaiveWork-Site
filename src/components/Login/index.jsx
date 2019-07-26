afbb4c0a5c0fd5c59f688a6a26160f52194d7221
David Alexander Leigh <daleighan@gmail.com>
2019-07-25 17:56:56 -0700

    Adding a prettierrc

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,33 +1,32 @@
-import React from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { updateForm } from "../../store/actions/userActions";
-import Form from "../Form";
+import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import Form from '../Form';
 
 let fields = [
   {
-    name: "email",
-    type: "text",
-    width: 3
+    name: 'email',
+    type: 'text',
+    width: 3,
   },
   {
-    name: "password",
-    type: "password",
-    width: 3
-  }
+    name: 'password',
+    type: 'password',
+    width: 3,
+  },
 ];
 
 function Login(props) {
   return (
     <div>
-      <Form fields={fields} formName={"authForm"} />
+      <Form fields={fields} formName={'authForm'} />
     </div>
   );
 }
 
 function mapStateToProps({ userReducer }) {
   return {
-    ...userReducer
+    ...userReducer,
   };
 }
 
@@ -37,5 +36,5 @@ function mapDispatchToProps() {
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  mapDispatchToProps,
 )(Login);
