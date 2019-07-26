193f1345518c8eb3e065b8ce8a600be6563c066a
David Alexander Leigh <daleighan@gmail.com>
2019-07-25 19:10:04 -0700

    fixing conifg styles

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 export default function() {
-  return process.env.NODE_ENV !== 'production' ? {
-    apiUrl: 'http://staging.waivecar.com:3080'
-  } : {
-    api:Url: 'https://api.waivecar.com'
-  };
+  return process.env.NODE_ENV !== 'production'
+    ? {
+        apiUrl: 'http://staging.waivecar.com:3080',
+      }
+    : {
+        apiUrl: 'https://api.waivecar.com',
+      };
 }
