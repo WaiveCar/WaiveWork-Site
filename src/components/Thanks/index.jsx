2188f01e743ce6815d5c6d31a9494527c4b80c50
David Alexander Leigh <daleighan@gmail.com>
2019-08-14 15:44:06 -0700

    Added text to the thanks page

--- src/components/Thanks/index.jsx
+++ src/components/Thanks/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
 
 function Thanks() {
-  return <div>Thanks</div>;
+  return (
+    <div>
+      <p>
+        Thanks for Signing up! Your information has been saved, and submitted to
+        insurance for a quote unique to you. Our staff will reach out to you by
+        email within the next 2 business days. If you don't hear anything back
+        by then, please email natosha@waive.car for an update
+      </p>
+
+      <p>
+        If you have any questions, dont hesitate to shoot us an email at
+        support@waive.car.
+      </p>
+
+      <p>
+        If you're interested in a partnership with us or advertising
+        opportunities, email us at info@waive.car.
+      </p>
+    </div>
+  );
 }
 
 export default Thanks;
