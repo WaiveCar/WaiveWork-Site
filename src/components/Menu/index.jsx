d91dc4e14ede697b52e602bc07c7c853ff3a8045
David Alexander Leigh <daleighan@gmail.com>
2019-08-14 19:35:22 -0700

    Did some initial work on the appearance of a menu button

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { logout } from '../../store/actions/userActions';
+import Bars from '../../svg/bars.svg';
+import './menu.scss';
 
-function Menu({ logout }) {
+function Menu({ logout, loggedIn }) {
   return (
     <div>
-      <div className="top-bar">top bar</div>
+      <div className="top-bar">
+        {loggedIn && (
+          <div className="menu-button">
+            <Bars className="menu-svg" />
+          </div>
+        )}
+      </div>
       <Link to={'/login'}>login</Link>
       <Link to={'/signup'}>signup</Link>
       <Link to={'/dashboard'}>dashboard</Link>
--- webpack.config.js
+++ webpack.config.js
@@ -30,10 +30,6 @@ module.exports = {
         ],
       },
       {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-      {
         test: /\.svg$/,
         use: [
           {
