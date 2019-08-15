f85057deb6ff454b385d548488209abb9a5ad156
David Alexander Leigh <daleighan@gmail.com>
2019-08-14 18:43:03 -0700

    Added a file that will contain the menu options

---
+++ src/components/Menu/menuLinks.js
@@ -0,0 +1,52 @@
+export default [
+  {
+    name: 'Current Booking',
+    children: [
+      {
+        name: 'Information',
+        href: '/booking',
+      },
+      {
+        name: 'Chargers',
+        href: '/chargers',
+      },
+      {
+        name: 'Registration',
+        href: '/registration',
+      },
+      {
+        name: 'Insurance',
+        href: '/insurance',
+      },
+      {
+        name: 'Rideshare Doc',
+        href: '/inspection',
+      },
+    ],
+  },
+  {
+    name: 'Billing Information',
+    children: [
+      {
+        name: 'Payments',
+        href: '/payments',
+      },
+      {
+        name: 'My Cards',
+        href: '/cards',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+  },
+  {
+    name: 'Account Info',
+    href: '/account',
+  },
+  {
+    name: 'Contact Info',
+    href: '/contact',
+  },
+];
