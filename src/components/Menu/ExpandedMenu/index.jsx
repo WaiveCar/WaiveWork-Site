373caf35b5e710ae4737e74399dd64891c04d08d
David Alexander Leigh <daleighan@gmail.com>
2019-08-16 19:00:29 -0700

    Mapping items onto the menu

--- src/components/Menu/ExpandedMenu/index.jsx
+++ src/components/Menu/ExpandedMenu/index.jsx
@@ -2,13 +2,28 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { hideMenu, toggleItem } from '../../../store/actions/menuActions';
+import { Link } from 'react-router-dom';
 import './expandedMenu.scss';
 
 function ExpandedMenu({ hideMenu, menuLinks }) {
-  console.log('menuLinks', menuLinks);
   return (
     <div className="outer-menu" onClick={() => hideMenu()}>
-      <div className="inner-menu" onClick={(e) => e.stopPropagation()}></div>
+      <div className="inner-menu" onClick={(e) => e.stopPropagation()}>
+        {menuLinks.map((item, i) => {
+          console.log(item, item.href);
+          return (
+            <div key={i}>
+              {item.href ? (
+                <Link to={item.href} onClick={() => hideMenu()}>
+                  {item.name}
+                </Link>
+              ) : (
+                <div>{item.name}</div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
