7bb7f51632da85078d153b45562862545797e6b4
David Alexander Leigh <daleighan@gmail.com>
2019-08-20 16:08:19 -0700

    Hopefully accounting for different file types

--- src/components/Doc/index.jsx
+++ src/components/Doc/index.jsx
@@ -3,17 +3,37 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 function Doc(props) {
-  console.log('hopefully logging file', props[`${props.type}File`]);
-  return (
-    props[`${props.type}File`] && (
-      <div>
-        {
-          <img
-            src={`https://waivecar-prod.s3.amazonaws.com/${props[`${props.type}File`].path}`}
-          />
-        }
-      </div>
-    )
+  const { type } = props;
+  return props[`${props.type}File`] ? (
+    <div>
+      {props[`${type}File`].mime !== 'image/jpeg' ? (
+        <div>
+          {props[`${type}File`].mime === 'application/pdf' ? (
+            <embed
+              style={{ maxWidth: '100%' }}
+              src={`http://docs.google.com/gview?url=http://waivecar-prod.s3.amazonaws.com/${props[`${type}File`].path}&embedded=true`}
+            />
+          ) : (
+            <video
+              style={{ maxWidth: '100%', maxHeight: '250px' }}
+              controls="controls"
+            >
+              <source
+                src={`http://waivecar-prod.s3.amazonaws.com/${props[`${type}File`].path}`}
+                type={'video/mp4'}
+              />
+            </video>
+          )}
+        </div>
+      ) : (
+        <img
+          style={{ maxWidth: '100%', maxHeight: '300px' }}
+          src={`http://waivecar-prod.s3.amazonaws.com/${props[`${type}File`].path}`}
+        />
+      )}
+    </div>
+  ) : (
+    <div>File not uploaded yet</div>
   );
 }
 
