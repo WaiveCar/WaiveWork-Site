fb5c43257400afb63d9563a44edffb1f5e59d5d0
David Alexander Leigh <daleighan@gmail.com>
2019-08-20 21:03:17 -0700

    The car should now be able to lock and unlock

--- src/store/actions/carActions.js
+++ src/store/actions/carActions.js
@@ -1,3 +1,16 @@
+import axios from 'axios';
+
 export const carCommand = (carId, command) => async (dispatch) => {
-  console.log('execute car command here');
+  try {
+    let response = await axios.get(`/cars/${carId}/${command}`);
+    dispatch({ type: 'UPDATE_CAR', payload: { car: response.data } });
+  } catch (e) {
+    dispatch({
+      type: 'SHOW_SNACKBAR',
+      payload: {
+        message: e.response ? e.response.data.message : e,
+        type: 'error',
+      },
+    });
+  }
 };
