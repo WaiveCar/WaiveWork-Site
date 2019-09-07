c51870c1b585fb8988908853d59fe9fa9c6dedd2
David Alexander Leigh <daleighan@gmail.com>
2019-09-06 18:13:53 -0700

    Began adding the password change form

---
+++ src/components/Form/passwordForm.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: 'old password',
+    formField: 'oldPassword',
+    type: 'password',
+    width: 8,
+  },
+  {
+    name: 'new password',
+    formField: 'newPassword',
+    type: 'password',
+    width: 8,
+  },
+  {
+    name: 'confirm new password',
+    formField: 'password',
+    type: 'password',
+    width: 8,
+  },
+];
