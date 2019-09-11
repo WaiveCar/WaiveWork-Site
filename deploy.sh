34557fb94636e826c0f8538723b80cd8633ddb13
David Alexander Leigh <daleighan@gmail.com>
2019-09-10 20:02:02 -0700

    The deploy script now invalidates old versions of objects

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-npm run build && aws s3 cp static s3://waivework/ --recursive
+npm run build 
+aws cloudfront create-invalidation --distribution-id E3K7J18T9N82X6 --paths '/*'  
+aws s3 cp static s3://waivework/ --recursive
