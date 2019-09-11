9e5a9db71c93b67fe26b65841c96c484408c9c53
David Alexander Leigh <daleighan@gmail.com>
2019-09-10 19:06:48 -0700

    Added a component for updating license information

---
+++ src/components/License/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { updateLicense } from '../../store/actions/userActions';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import Form from '../Form';
+
+function License({ licenseFormFields, user, updateLicense }) {
+  return (
+    <div className="container">
+      <Form
+        fields={personalFormFields}
+        title={'Update License'}
+        formName={'licenseForm'}
+        onSubmit={(form) => updateLicense(form)}
+        submitName={'Update'}
+        formWidth={'60%'}
+      />
+    </div>
+  );
+}
+
+function mapStateToProps({ userReducer, formReducer }) {
+  return { ...userReducer, ...formReducer };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({}, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(License);
