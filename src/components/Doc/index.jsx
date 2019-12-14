128631a6bc21b118d73bf62570e3e0d5668ed3ef
David Alexander Leigh <daleighan@gmail.com>
2019-12-13 18:31:26 -0800

    Adding better handling of missing docs

--- src/components/Doc/index.jsx
+++ src/components/Doc/index.jsx
@@ -5,52 +5,64 @@ import moment from 'moment';
 import './doc.scss';
 
 function Doc(props) {
-  const { type, userResourcesLoaded } = props;
-  const currentFile = props[`${props.type}File`];
-  return currentFile ? (
+  const { type, userResourcesLoaded, currentBooking } = props;
+  const currentFile = props[`${type}File`];
+  return (
     <div className="container mt-4">
       <h1>Your {type}</h1>
-      <h5 className="d-flex justify-content-between">
-        <div>(expires {moment(currentFile.comment).format('MM/DD/YYYY')})</div>
-        <a
-          href={`https://waivecar-prod.s3.amazonaws.com/${currentFile.path}`}
-          target="_blank"
-        >
-          Download
-        </a>
-      </h5>
-      <div className="row d-flex justify-content-center mt-4">
-        {currentFile.mime !== 'image/jpeg' ? (
-          currentFile.mime === 'application/pdf' ? (
-            <embed
-              className={'doc-embed'}
-              src={`http://docs.google.com/gview?url=http://waivecar-prod.s3.amazonaws.com/${currentFile.path}&embedded=true`}
-            />
-          ) : (
-            <video classname={'doc-video'} controls="controls">
-              <source
+      {currentFile ? (
+        <div>
+          <h5 className="d-flex justify-content-between">
+            <div>
+              (expires {moment(currentFile.comment).format('MM/DD/YYYY')})
+            </div>
+            <a
+              href={`https://waivecar-prod.s3.amazonaws.com/${currentFile.path}`}
+              target="_blank"
+            >
+              Download
+            </a>
+          </h5>
+          <div className="row d-flex justify-content-center mt-4">
+            {currentFile.mime !== 'image/jpeg' ? (
+              currentFile.mime === 'application/pdf' ? (
+                <embed
+                  className={'doc-embed'}
+                  src={`http://docs.google.com/gview?url=http://waivecar-prod.s3.amazonaws.com/${currentFile.path}&embedded=true`}
+                />
+              ) : (
+                <video classname={'doc-video'} controls="controls">
+                  <source
+                    src={`http://waivecar-prod.s3.amazonaws.com/${currentFile.path}`}
+                    type={'video/mp4'}
+                  />
+                </video>
+              )
+            ) : (
+              <img
+                className={'doc-image'}
                 src={`http://waivecar-prod.s3.amazonaws.com/${currentFile.path}`}
-                type={'video/mp4'}
               />
-            </video>
-          )
-        ) : (
-          <img
-            className={'doc-image'}
-            src={`http://waivecar-prod.s3.amazonaws.com/${currentFile.path}`}
-          />
-        )}
-      </div>
-    </div>
-  ) : (
-    <div>
-      <div>File not uploaded yet</div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div>
+          {!currentBooking &&
+          type !== 'registration' &&
+          type !== 'inspection' ? (
+            <h5>File not uploaded</h5>
+          ) : (
+            <h5>Not currently in a booking</h5>
+          )}
+        </div>
+      )}
     </div>
   );
 }
 
-function mapStateToProps({ userReducer, carReducer }) {
-  return { ...userReducer, ...carReducer };
+function mapStateToProps({ userReducer, carReducer, bookingReducer }) {
+  return { ...userReducer, ...carReducer, ...bookingReducer };
 }
 
 function mapDispatchToProps(dispatch) {
