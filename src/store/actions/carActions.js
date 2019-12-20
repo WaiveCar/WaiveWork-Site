eb80a124fa2ceda527ee568e39d2052bff4845d9
David Alexander Leigh <daleighan@gmail.com>
2019-12-20 14:07:26 -0800

    Messing with the z-index for the loading comp

--- src/store/actions/carActions.js
+++ src/store/actions/carActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { showSnackbar } from './snackbarActions';
 import { getBookingStats } from './bookingActions';
+import { toggleLoading } from './menuActions';
 
 export const updateCar = (car) => (dispatch) => {
   return dispatch({
@@ -22,9 +23,12 @@ export const getCarHistory = (carId, booking) => async (dispatch) => {
 
 export const carCommand = (carId, command) => async (dispatch) => {
   try {
+    await dispatch(toggleLoading());
     let response = await axios.get(`/cars/${carId}/${command}`);
     await dispatch({ type: 'UPDATE_CAR', payload: { car: response.data } });
+    await dispatch(toggleLoading());
   } catch (e) {
+    await dispatch(toggleLoading());
     return dispatch(
       showSnackbar(e.response ? e.response.data.message : e, 'error'),
     );
