090e7d6b4e4da3247b0730ee9f6d3bfe8afcf967
David Alexander Leigh <daleighan@gmail.com>
2020-01-13 13:58:12 -0800

    The deploy script now points to prod server

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-npm run build 
-aws cloudfront create-invalidation --distribution-id E3K7J18T9N82X6 --paths '/*'  
-aws s3 cp static s3://waivework/ --recursive
+npm run build
+scp -r ./static/* waivework:/var/www/waivework/  
