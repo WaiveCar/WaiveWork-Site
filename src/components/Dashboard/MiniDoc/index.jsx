fbe1f6585a400311132d44c1e7af72b099a138fa
David Alexander Leigh <daleighan@gmail.com>
2020-01-14 18:30:27 -0800

    Adding sizing to the MiniDoc

--- src/components/Dashboard/MiniDoc/index.jsx
+++ src/components/Dashboard/MiniDoc/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import './miniDoc.scss';
 
 function MiniDoc({ file }) {
   return (
-    <div>
+    <div className={'mini-doc d-flex justify-content-center'}>
       {file ? (
         file.mime === 'application/pdf' ? (
           <embed
-            className={'doc-embed'}
             src={`http://docs.google.com/gview?url=http://waivecar-prod.s3.amazonaws.com/${file.path}&embedded=true`}
+            className={'mini-embed'}
           />
         ) : (
           <img
