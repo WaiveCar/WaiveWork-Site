bfe58105d03e7299f144abc5b77e6bbb3b9af73a
David Alexander Leigh <daleighan@gmail.com>
2020-01-21 14:01:22 -0800

    Added a new google maps api key

--- src/config.js
+++ src/config.js
@@ -2,10 +2,10 @@ export default (() => {
   return process.env.NODE_ENV !== 'production'
     ? {
         apiUrl: 'http://127.0.0.1:3080',
-        mapsKey: 'AIzaSyDuTxwQN4WKCktkzkLTHZSD7EzHvCn3WHs',
+        mapsKey: 'AIzaSyBQysUfVLDsR8aYHZBQ9epqpGAQ-LZ1bTw',
       }
     : {
         apiUrl: 'https://api.waivecar.com',
-        mapsKey: 'AIzaSyDuTxwQN4WKCktkzkLTHZSD7EzHvCn3WHs',
+        mapsKey: 'AIzaSyBQysUfVLDsR8aYHZBQ9epqpGAQ-LZ1bTw',
       };
 })();
