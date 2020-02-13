8c35108aa7b320dd622f5731ae908a0003736508
David Alexander Leigh <daleighan@gmail.com>
2020-02-12 16:52:18 -0800

    Removing unused items

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Parser from 'html-react-parser';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
