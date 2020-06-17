9b6e34c75e8f260712e7fda7fbe18c831fc33bb8
David Alexander Leigh <daleighan@gmail.com>
2020-06-17 14:39:24 -0700

    Adding some styles to car search

--- src/components/Dashboard/BookCars/index.jsx
+++ src/components/Dashboard/BookCars/index.jsx
@@ -3,28 +3,41 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { carSearch } from '../../../store/actions/carActions';
 import { createBooking } from '../../../store/actions/bookingActions';
+import './bookCars.scss';
 
 function BookCars({ user, carSearch, searchResults, createBooking }) {
   let [searchText, setText] = useState('');
   return (
-    <div className="card booking-card">
+    <div className="card booking-card book-cars">
       <div className="card-body">
-        <h5 className="card-title">Book A Car:</h5>
-        <div>
-          <input type="text" onChange={(e) => setText(e.target.value)} />
-          <button
-            onClick={() => carSearch(searchText, user)}
-            className="btn btn-primary"
-          >
-            Search
-          </button>
-        </div>
+        <h5 className="card-title">Search For Cars:</h5>
+        <form>
+          <div className="form-group">
+            <div className="row">
+              <input
+                type="text"
+                className="col-9 form-control"
+                onChange={(e) => setText(e.target.value)}
+              />
+              <button
+                className="col-3"
+                onClick={(e) => {
+                  e.preventDefault();
+                  carSearch(searchText, user);
+                }}
+                className="btn btn-primary"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
         {searchResults.map((each, i) => (
           <div key={i} className="d-flex justify-content-between">
             <div>{each.license}</div>
             <button
               className="btn btn-primary"
-              onClick={() => createBooking(each.id, user)}
+              onClick={(e) => createBooking(each.id, user)}
             >
               Book
             </button>
